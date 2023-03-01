455f6c3daa97efa85ee8c3a80a4854484dae6c48
sosroInSpace <mathewbowyer@hotmail.com>
2023-03-01 10:21:29 +0800

    updated some tests

--- src/components/NotFoundPage/NotFoundPage.test.js
+++ src/components/NotFoundPage/NotFoundPage.test.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import NotFoundPage from "./NotFoundPage";
@@ -11,8 +10,11 @@ describe("NotFoundPage", () => {
       </MemoryRouter>
     );
 
-    const notFoundHeading = getByText(/404 - Not Found/i);
+    const notFoundHeading = getByText(/404/i);
     expect(notFoundHeading).toBeInTheDocument();
+
+    const notFoundMessage = getByText(/Sorry this page does not exist/i);
+    expect(notFoundMessage).toBeInTheDocument();
 
     const linkToHomePage = getByText(/Return to Home Page/i);
     expect(linkToHomePage).toBeInTheDocument();
--- src/components/RootPage/RootPage.test.js
+++ src/components/RootPage/RootPage.test.js
@@ -1,15 +1,144 @@
-import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
-import SpellsPage from "./SpellsPage";
+import { MemoryRouter } from "react-router-dom";
+import configureMockStore from "redux-mock-store";
+import RootPage from "./RootPage";
 
-test("renders learn react link", () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <RootPage />
-    </Provider>
-  );
+const mockStore = configureMockStore([]);
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
-});
+describe("RootPage component", () => {
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({
+      spells: {
+        spells: [
+          {
+            index: "acid-arrow",
+            name: "Acid Arrow",
+            url: "/api/spells/acid-arrow",
+          },
+          {
+            index: "bless",
+            name: "Bless",
+            url: "/api/spells/bless",
+          },
+        ],
+        spell: null,
+        storage: null,
+        favourites: [],
+        status: "idle",
+        error: null,
+      },
+    });
+  });
+
+  it("should render Spell List header", () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <RootPage />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    expect(screen.getByRole("heading", { name: "Spell List" })).toBeInTheDocument();
+  });
+
+  it("should render My Favourite Spells header when favourites prop is passed", () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <RootPage favourites />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    expect(
+      screen.getByRole("heading", { name: "My Favourite Spells" })
+    ).toBeInTheDocument();
+  });
+
+  it("should render loader when status is loading", () => {
+    store = mockStore({
+      spells: {
+        spells: [],
+        spell: null,
+        storage: null,
+        favourites: [],
+        status: "loading",
+        error: null,
+      },
+    });
+
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <RootPage />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    expect(screen.getByRole("status")).toBeInTheDocument();
+  });
+
+  it("should render No Spells Found message when status is succeeded but spells list is empty", () => {
+    store = mockStore({
+      spells: {
+        spells: [],
+        spell: null,
+        storage: null,
+        favourites: [],
+        status: "succeeded",
+        error: null,
+      },
+    });
+
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <RootPage />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    expect(screen.getByText(/No Spells Found/)).toBeInTheDocument();
+  });
+
+  it("should render error message and retry button when error is present", () => {
+    store = mockStore({
+      spells: {
+        spells: [],
+        spell: null,
+        storage: null,
+        favourites: [],
+        status: "failed",
+        error: {
+          message: "Failed to fetch spells",
+        },
+      },
+    });
+
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <RootPage />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    expect(screen.getByText(/Failed to fetch spells/)).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Click to Retry" })).toBeInTheDocument();
+  });
+
+  it("should render spells list when status is succeeded and spells list is not empty", () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <RootPage />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    expect(screen.getByText(/Total Count:/)).toBeInTheDocument();
